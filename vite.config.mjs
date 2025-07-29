68bed3b32b43aaff3c283546cb9c5fbcd657ee4b
Elvis <elvisboateng557@gmail.com>
2025-07-29 11:15:39 -1200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(async () => {
   const tailwindcss = await import('@tailwindcss/vite')
-  
+ 
   return {
     plugins: [
       react(),
