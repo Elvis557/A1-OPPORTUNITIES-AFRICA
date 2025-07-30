4af5b661c29e623cab8e7f7bf3f00840c31beed3
Elvis <elvisboateng557@gmail.com>
2025-07-30 08:47:05 -1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(async () => {
-  const tailwindcss = await import('@tailwindcss/vite')
-  
-  return {
-    plugins: [
-      react(),
-      tailwindcss.default()
-    ],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
