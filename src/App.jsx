e9a90f3e5924af44ac3bba8bcba1f89f602dbc20
Elvis <elvisboateng557@gmail.com>
2025-08-04 02:07:49 -1200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import AboutUs from './pages/AboutUs';
 import Servicespage from './pages/Servicespage';
-import NotFound from './pages/NotFound';
+import NotFound from './pages/Notfound';
 
 
 function App() {
