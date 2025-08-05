9925ede78e7d5f4ca500ce10afe9835ad0c2405d
Elvis <elvisboateng557@gmail.com>
2025-08-05 11:58:33 -1200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Footer from '../components/Footer';
-
+import WhatsAppButton from '../components/WhatsAppButton'; 
 
 const Home = () => {
   return (
     <>
-        <Navbar />
-        <Hero />
-        <Services />
-        <Footer />
-    </>
-  )
-}
+      <Navbar />
+      <Hero />
+      <Services />
 
-export default Home
+      <WhatsAppButton /> 
+
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
