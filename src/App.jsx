e0e99df6b26ab387ebdb96ba7a7964f4ec323a1b
Elvis <elvisboateng557@gmail.com>
2025-08-22 05:02:21 -1200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import AboutUs from "./pages/AboutUs";
 import Servicespage from "./pages/Servicespage";
-import Notfound from "./pages/NotFound.jsx";
+import NotFound from "./pages/NotFound.jsx"; // ✅ Fixed casing
 
 // Admin Pages
 import LoginPage from "./pages/LoginPage";
