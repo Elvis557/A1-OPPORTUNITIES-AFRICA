f5cc543b2c4bbab2e2b61737de3a1c6c6df11ffb
Zephaniah Winsakiya Ayuah <zephaniahayuah@gmail.com>
2025-08-22 20:19:46 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import AboutUs from "./pages/AboutUs";
 import Servicespage from "./pages/Servicespage";
-import NotFound from "./pages/Notfound";
+import NotFound from "./pages/NotFound";
 
 // Admin Pages
 import LoginPage from "./pages/LoginPage";
