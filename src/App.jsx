3fe099d47e2b2b3b50feeccc13db4ae945312bb4
Elvis <elvisboateng557@gmail.com>
2025-08-22 02:44:14 -1200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import AboutUs from './pages/AboutUs';
-import Servicespage from './pages/Servicespage';
-import NotFound from "./pages/NotFound"
+// Public Pages
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import AboutUs from "./pages/AboutUs";
+import Servicespage from "./pages/Servicespage";
+import NotFound from "./pages/Notfound";
+
+// Admin Pages
+import LoginPage from "./pages/LoginPage";
+import AdminDashboard from "./pages/AdminDashboard";
+import PrivateRoute from "./routes/PrivateRoutes";
 
 function App() {
   return (
     <Router>
+      <Navbar />
 
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path='/services' element={<Servicespage/>} />
-        <Route path='*' element={<NotFound/>} />
+        <Route path="/services" element={<Servicespage />} />
+
+        {/* Admin Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Catch-all */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      
+
+      <Footer />
     </Router>
-      
   );
 }
 
