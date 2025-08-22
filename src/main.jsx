6c4a9950e0a32a0c5d7ceeabac4f4b5b954b6b07
Elvis <elvisboateng557@gmail.com>
2025-08-22 02:44:47 -1200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react"; // <-- import ChakraProvider
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider> 
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
